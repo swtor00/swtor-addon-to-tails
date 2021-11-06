d17e01349826cdbe35a3cd6d5a590db7481edbc0
0.60 <amnesia@localhost>
2021-11-06 10:21:58 +0000

    0.60

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -4,52 +4,66 @@
 #########################################################
 # AUTHORS : swtor00                                     #
 # EMAIL   : swtor00@protonmail.com                      #
-# OS      : Tails 4.14 or higher                        #
+# OS      : Tails 4.23 or higher                        #
 #                                                       #
-# VERSION : 0.52                                        #
+# VERSION : 0.60                                        #
 # STATE   : BETA                                        #
 #                                                       #
 # This shell script is part of the swtor-addon-to-tails #
 #                                                       #
-# DATE    : 25-12-2020                                  #
+# DATE    : 04-11-2021                                  #
 # LICENCE : GPL 2                                       #
 #########################################################
 # Github-Homepage :                                     #
 # https://github.com/swtor00/swtor-addon-to-tails       #
 #########################################################
 
+# the following global variables are exported from the main
+# script swtor-menu.sh and are visible here.
+#
+# IMPORT_BOOKMAKRS
+# GUI_LINKS
+# CHECK_UPDATE
+# BACKUP_FIXED_PROFILE
+# BACKUP_APT_LIST
+# TERMINAL_VERBOSE
+# TIMEOUT_TB
+
+
 # This script has to be run only once ... no more
 
-if [ -f ~/swtor_init ]
-   then
-   sleep 5 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="The command \"startup.sh\" was allready executed!" > /dev/null 2>&1) 
+if [ -f ~/swtor_init ] ; then
+   if [ $TERMINAL_VERBOSE == "1" ] ; then
+      sleep 5 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="The command \"startup.sh\" was allready executed!" > /dev/null 2>&1)
+   fi
    exit 0
 fi
 
 
 # Has setup ever run on this tails system ?
 
-if [ !  -f ~/Persistent/swtor-addon-to-tails/setup ]
-   then
-       sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Execute the command \"setup-swtor.sh\" first !" > /dev/null 2>&1)
-       exit 1
-
+if [ !  -f ~/Persistent/swtor-addon-to-tails/setup ] ; then
+   sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Please execute the command \"setup-swtor.sh\" first !" > /dev/null 2>&1)
+   exit 1
 else
-    echo step 00.
-    echo "setup-swtor has ben executed once .... "
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+         echo "setup-swtor has ben executed once ....  "
+         echo done
+    fi
 fi
 
-# Check to see if ONION Network is allready runnig ....
 
+# Check to see if ONION Network is allready runnig ....
 
 sleep 2 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Testing the internet-connection !" > /dev/null 2>&1)
 
 curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ -m 6 | grep -m 1 Congratulations > /dev/null
 if [ $? -eq 0 ] ; then
-   echo step 01a.
-   echo TOR is up and running and we can continue with the execution of the script ....
-   echo done
+   if [ $TERMINAL_VERBOSE == "1" ] ; then
+      echo step 01a.
+      echo TOR is up and running and we can continue with the execution of the script ....
+      echo done
+   fi
 else
   sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="TOR is not ready or no internet connection" > /dev/null 2>&1)
   exit 1
@@ -60,69 +74,83 @@ fi
 
 sleep 2 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Testing ssh files for the addon ..." > /dev/null 2>&1)
 
-if [ -z "$(ls -A /home/amnesia/.ssh )" ]; then
+if [ -z "$(ls -A /home/amnesia/.ssh )" ] ; then
    sleep 8 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="The directory /home/amnesia/.ssh is empty.\n\nThis addons needs a predefined SSH connection" > /dev/null 2>&1)
-fi 
+   exit
+fi
 
 sleep 2 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Testing the installed additional software !" > /dev/null 2>&1)
 
 # test for installed yad command from persistent volume
 
-if grep -q "status installed yad" /var/log/dpkg.log
-then
-    echo step 01b.
-    echo "yad is installed .... "
-    echo done
+if grep -q "status installed yad" /var/log/dpkg.log ; then
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01b.
+       echo "yad is installed .... "
+       echo done
+    fi
 else
-    echo step 01b.
-    echo "yad is not installed .... "
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01b.
+       echo "yad is not installed .... "
+       echo done
+    fi
     sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Tor is ready but yad is not installed by now !" > /dev/null 2>&1)
     exit 1
 fi
 
+
 # test for installed sshpass command from persistent volume
 
-if grep -q "status installed sshpass" /var/log/dpkg.log
-then
-    echo step 01c.
-    echo "sshpass is installed .... "
-    echo done
+if grep -q "status installed sshpass" /var/log/dpkg.log ; then
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01c.
+       echo "sshpass is installed .... "
+       echo done
+    fi
 else
-    echo step 01c.
-    echo "sshpass is not installed .... "
-    echo done
-        sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Tor is ready but sshpass is not installed by now !" > /dev/null 2>&1)
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01c.
+       echo "sshpass is not installed .... "
+       echo done
+    fi
+       sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Tor is ready but sshpass is not installed by now !" > /dev/null 2>&1)
     exit 1
 fi
 
 
 # test for installed html2text command from persistent volume
 
-if grep -q "status installed html2text" /var/log/dpkg.log
-then
-    echo step 01d.
-    echo "html2text is installed .... "
-    echo done
+if grep -q "status installed html2text" /var/log/dpkg.log ; then 
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01d.
+       echo "html2text is installed .... "
+       echo done
+    fi
 else
-    echo step 01d.
-    echo "html2text is not installed .... "
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01d.
+       echo "html2text is not installed .... "
+       echo done
+    fi
     sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Tor is ready but html2text is not installed by now !" > /dev/null 2>&1)
     exit 1
 fi
 
 # test for installed chromium command from persistent volume
 
-if grep -q "status installed chromium" /var/log/dpkg.log
-then
-    echo step 01e.
-    echo "chromium is installed .... "
-    echo done
+if grep -q "status installed chromium" /var/log/dpkg.log ; then
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01e.
+       echo "chromium is installed .... "
+       echo done
+    fi
 else
-    echo step 01e.
-    echo "chromium is not installed .... "
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01e.
+       echo "chromium is not installed .... "
+       echo done
+    fi
     sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Tor is ready but chromium is not installed by now !" > /dev/null 2>&1)
     exit 1
 fi
@@ -130,15 +158,18 @@ fi
 
 # test for installed chromium-sandbox command from persistent volume
 
-if grep -q "status installed chromium-sandbox" /var/log/dpkg.log
-then
-    echo step 01f.
-    echo "chromium-sandbox is installed .... "
-    echo done
+if grep -q "status installed chromium-sandbox" /var/log/dpkg.log ; then
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01f.
+       echo "chromium-sandbox is installed .... "
+       echo done
+    fi
 else
-    echo step 01f.
-    echo "chromium-sandbox is not installed .... "
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 01f.
+       echo "chromium-sandbox is not installed .... "
+       echo done
+    fi
     sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Tor is ready but chromium-sandbox is not installed by now !" > /dev/null 2>&1)
     exit 1
 fi
@@ -147,17 +178,18 @@ fi
 
 # Do we have a freezed Tails ?
 
-if [ -f ~/Persistent/swtorcfg/freezed.cgf ]
-   then
+if [ -f ~/Persistent/swtorcfg/freezed.cgf ] ; then 
        tails-version > ~/Persistent/scripts/current
-
        if diff -q ~/Persistent/swtorcfg/freezed.cgf ~/Persistent/scripts/current ;then
-          echo step 02
-          echo this system was freezed with the same version of tails that it was created  ...
-          echo done
+          if [ $TERMINAL_VERBOSE == "1" ] ; then
+             echo step 02
+             echo this system was freezed with the same version of tails that it was created  ...
+             echo done
+          fi
+          sleep 1
        else
             # Houston ... We have a problem
-            # You should not run this addon with a freezed system from a older tails version than the current one ...
+            # You should not run this addon with a freezed system from a older tails version than the current one used
 
             rm -rf /live/persistence/TailsData_unlocked/dotfiles/.config > /dev/null 2>&1
             rm -rf /live/persistence/TailsData_unlocked/dotfiles/Desktop > /dev/null 2>&1
@@ -174,16 +206,17 @@ fi
 
 echo _123UUU__ | sudo -S /bin/bash > ~/Persistent/scripts/test_admin 2>&1
 
-if grep -q "is not allowed to execute" ~/Persistent/scripts/test_admin
- then
+if grep -q "is not allowed to execute" ~/Persistent/scripts/test_admin ; then 
      rm ~/Persistent/scripts/test_admin > /dev/null 2>&1
      zenity --info --width=600 --text="\nYou have to set a administration password on\n the greeting-screen of Tails!" 
      exit 1
 else
     rm ~/Persistent/scripts/test_admin > /dev/null 2>&1
-    echo step 04
-    echo we have a administration password
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 04
+       echo we have a administration password
+       echo done
+    fi
 fi
 
 sleep 5 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Checking for updates\nDepending on the configuration of swtor.cfg" > /dev/null 2>&1)
@@ -192,10 +225,11 @@ sleep 5 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="In
 # Check for updates on demand if CHECK-UPDATE:YES is set of inside swtor.cfg
 # The default Value of this setting is : NO
 
-if grep -q CHECK-UPDATE:YES ~/Persistent/swtorcfg/swtor.cfg
- then
+if grep -q CHECK-UPDATE:YES ~/Persistent/swtorcfg/swtor.cfg ; then
 
-    echo "Checking for updates on github"
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo "Checking for updates on github"
+    fi
 
     # If you don't like this behavior on startup, you should open the
     # configuration file ~/Persistent/swtorcfg/swtor.cfg and set the option
@@ -204,7 +238,6 @@ if grep -q CHECK-UPDATE:YES ~/Persistent/swtorcfg/swtor.cfg
     # of the addon.
 
     sleep 4 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Checking for updates. Please wait ..." > /dev/null 2>&1)
-
 
     # We contact github to see what version is stored over there ....
 
@@ -215,34 +248,40 @@ if grep -q CHECK-UPDATE:YES ~/Persistent/swtorcfg/swtor.cfg
 
     # Comparing the remote and the local version of the scirpt..
 
-    echo REMOTE-VERSION [$REMOTE] LOCAL-VERSION [$LOCAL]
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo REMOTE-VERSION [$REMOTE] LOCAL-VERSION [$LOCAL]
+    fi
 
-    if [ "$REMOTE" == "$LOCAL" ]
-    then
-        echo step 05
-        echo "no updates found"
-        echo done
+    if [ "$REMOTE" == "$LOCAL" ] ; then
+       if [ $TERMINAL_VERBOSE == "1" ] ; then
+          echo step 05
+          echo "no updates found"
+          echo done
+       fi
     else
 
          # Is this script controlled with git or not ?
 
-         if [ ! -d ~/Persistent/swtor-addon-to-tails/.git ]
-         then
+         if [ ! -d ~/Persistent/swtor-addon-to-tails/.git ] ; then 
              yad --title="Information " --width=400 --height=100 --center \
              --text="\n\n Addon has no .git directory.\n This means that this addon isn't controlled by git."
              exit 1
          fi
          sleep 4 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Update found ... install the update ..." > /dev/null 2>&1)       
          ./udpate.sh
-         echo step 05
-         echo "Update for addon installed"
-         echo done
+         if [ $TERMINAL_VERBOSE == "1" ] ; then
+            echo step 05
+            echo "Update for addon installed"
+            echo done
+         fi
     fi
     rm ~/Persistent/scripts/REMOTE-VERSION > /dev/null 2>&1
 else
-    echo step 05
-    echo "Not checking for updates of the script."
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+        echo step 05
+        echo "Not checking for updates of the script."
+        echo done
+    fi
 fi
 
 sleep 4 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Clean up all log-files" > /dev/null 2>&1)
@@ -254,18 +293,15 @@ rm -rf /home/amnesia/Persistent/swtorcfg/log/*.* > /dev/null 2>&1
 
 # cleanup all browser-settings and extract all settings from tar file
 
-if [ -d /home/amnesia/Persistent/settings/1  ]
-then
+if [ -d /home/amnesia/Persistent/settings/1  ] ; then
    rm -rf  ~/Persistent/settings/1 >/dev/null 2>&1
 fi
 
-if [ -d /home/amnesia/Persistent/settings/2  ]
-then
+if [ -d /home/amnesia/Persistent/settings/2  ] ; then
   rm -rf  ~/Persistent/settings/2 >/dev/null 2>&1
 fi
 
-if grep -q BROWSER-SOCKS5:YES ~/Persistent/swtorcfg/swtor.cfg
-   then
+if grep -q BROWSER-SOCKS5:YES ~/Persistent/swtorcfg/swtor.cfg ; then
        # Extract the default directorys 1 & 2
 
        cd ~/Persistent/settings
@@ -274,8 +310,7 @@ fi
 
 # Test the state of the connection
 
-if [ -f /home/amnesia/Persistent/scripts/state/online ]
-then
+if [ -f /home/amnesia/Persistent/scripts/state/online ] ; then
     cd /home/amnesia/Persistent/scripts/state
     rm online
 fi
@@ -284,14 +319,12 @@ fi
 
 cd /home/amnesia/Persistent/scripts
 
-if [ -f /home/amnesia/Persistent/scripts/password ]
-then
-    rm password
+if [ -f /home/amnesia/Persistent/scripts/password ] ; then
+   rm password
 fi
 
-if [ -f /home/amnesia/Persistent/scripts/password_correct ]
-then
-    rm password_correct
+if [ -f /home/amnesia/Persistent/scripts/password_correct ] ; then
+   rm password_correct
 fi
 
 
@@ -306,9 +339,11 @@ if [ "$password" == "" ];then
    exit 1
 fi
 
-echo step 06
-echo checking password
-echo done
+if [ $TERMINAL_VERBOSE == "1" ] ; then
+   echo step 06
+   echo checking password
+   echo done
+fi
 
 sleep 4 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Testing administration password ...." > /dev/null 2>&1)
 
@@ -319,33 +354,36 @@ gnome-terminal --window-with-profile=Unnamed -x bash -c /home/amnesia/Persistent
 
 # In the case of the file password_correct file has a size of 0 bytes ... The entered password wasn't correct 
 
-if [ -s /home/amnesia/Persistent/scripts/password_correct ]
-then
+if [ -s /home/amnesia/Persistent/scripts/password_correct ] ; then 
     sleep 4 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Password was not correct .." > /dev/null 2>&1)
     rm password
     rm password_correct
     exit 1
 else
-    echo step 07
-    echo password is correct
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 07
+       echo password is correct
+       echo done
+    fi
 fi
-
 
 sleep 4 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Changing firewall rules to build a socks5 server  ..." > /dev/null 2>&1) 
 
 # change firewall for a ssh-socks5-connection
 
-if grep -q BROWSER-SOCKS5:YES ~/Persistent/swtorcfg/swtor.cfg
-   then
-   cat password | sudo -S iptables -I OUTPUT -o lo -p tcp --dport 9999 -j ACCEPT
-   cat password | sudo -S apt autoremove --yes
-   echo step 08
-   echo changing iptables firewall to accept socks5 connections
-   echo autoremove old unused packages
-   echo done
+if grep -q BROWSER-SOCKS5:YES ~/Persistent/swtorcfg/swtor.cfg ; then
+   cat password | sudo -S iptables -I OUTPUT -o lo -p tcp --dport 9999 -j ACCEPT  > /dev/null 2>&1
+   cat password | sudo -S apt autoremove --yes  > /dev/null 2>&1
+   if [ $TERMINAL_VERBOSE == "1" ] ; then
+      echo step 08
+      echo changing iptables firewall to accept socks5 connections
+      echo autoremove old unused packages
+      echo done
+   fi
 else
-   echo Browser-socks5 not selected
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo Browser-socks5 not selected
+    fi
 fi
 
 # Creating Desktop link
@@ -355,51 +393,55 @@ sleep 2 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="In
 # Make symbolic links on the desktop for the main menu
 # This depends on the setting GUI-LINKS:YES and BROWSER-SOCKS5:YES inside of swtor.cfg
 
-if grep -q BROWSER-SOCKS5:YES ~/Persistent/swtorcfg/swtor.cfg
-   then
-   if grep -q GUI-LINKS:YES ~/Persistent/swtorcfg/swtor.cfg
-      then
+if grep -q BROWSER-SOCKS5:YES ~/Persistent/swtorcfg/swtor.cfg ; then
+   if grep -q GUI-LINKS:YES ~/Persistent/swtorcfg/swtor.cfg ; then
           cd ~/Persistent/scripts
 
           if [ ! -L ~/Desktop/swtor-menu.sh ]
              then
              ln -s ~/Persistent/scripts/swtor-menu.sh ~/Desktop/swtor-menu.sh
-             echo step 08
-             echo symlink on desktop created
-             echo done
+             if [ $TERMINAL_VERBOSE == "1" ] ; then
+                echo step 08
+                echo symlink on desktop created
+                echo done
+             fi
           else
-             echo step 08
-             echo symlink on desktop allready exist
-             echo done
+              if [ $TERMINAL_VERBOSE == "1" ] ; then
+                 echo step 08
+                 echo symlink on desktop allready exist
+                 echo done
+              fi
           fi
    fi
 fi
 
-if grep -q GUI-LINKS:NO  ~/Persistent/swtorcfg/swtor.cfg
-   then
-   echo step 08
-   echo no symlink on desktop created .. setting GUI-LINKS:NO
-   echo done
+if grep -q GUI-LINKS:NO  ~/Persistent/swtorcfg/swtor.cfg ; then
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 08
+       echo no symlink on desktop created .. setting GUI-LINKS:NO
+       echo done
+   fi
 fi
 
 
 # We don't need longer the stored administration password
 
-if [ -f /home/amnesia/Persistent/scripts/password ]
-then
+if [ -f /home/amnesia/Persistent/scripts/password ] ; then
     cd /home/amnesia/Persistent/scripts
     rm password
     rm password_correct
-    echo step 09
-    echo removing password
-    echo done
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo step 09
+       echo removing password
+       echo done
+    fi
 fi
 
 
 echo 1 > /home/amnesia/Persistent/scripts/state/offline
 
 
-# We are finished here , signal with Error Code 0
+# We are done here , signal with Error Code 0
 
 echo 1 > ~/swtor_init
 
--- scripts/swtor-menu.sh
+++ scripts/swtor-menu.sh
@@ -11,52 +11,116 @@
 #                                                       #
 # Main-Menu of of the swtor-addon-to-tails              #
 #                                                       #
-# DATE    : 25-12-2020                                  #
+# DATE    : 01-11-2021                                  #
 # LICENCE : GPL 2                                       #
 #########################################################
 # Github-Homepage :                                     #
 # https://github.com/swtor00/swtor-addon-to-tails       #
 #########################################################
 
-# Create lockdir ....
+
+if grep -q "IMPORT-BOOKMARKS:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export IMPORT_BOOKMAKRS="1"
+else
+   export IMPORT_BOOKMAKRS="0"
+fi
+
+if grep -q "GUI-LINKS:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export GUI_LINKS="1"
+else
+   export GUI_LINKS="0"
+fi
+
+if grep -q "CHECK-UPDATE:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export CHECK_UPDATE="1"
+else
+   export CHECK_UPDATE="0"
+fi
+
+if grep -q "BACKUP-FIXED-PROFILE:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export BACKUP_FIXED_PROFILE="1"
+else
+   export BACKUP_FIXED_PROFILE="0"
+fi
+
+if grep -q "BACKUP_APT_LIST:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export BACKUP_APT_LIST="1"
+else
+     export BACKUP_APT_LIST="0"
+fi
+
+if grep -q "TERMINAL-VERBOSE:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export TERMINAL_VERBOSE="1"
+else
+     export TERMINAL_VERBOSE="0"
+fi
+
+export TIMEOUT_TB=$(grep TIMEOUT ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg | sed 's/[A-Z:-]//g')
+
+
+
+# Creating the lockdirectory ....
 
 lockdir=~/Persistent/scripts/menu.lock
-if mkdir "$lockdir"
-   then    # directory did not exist, but was created successfully
-       echo >&2 "successfully acquired lock: $lockdir"
-   else    # failed to create the directory, presumably because it already exists
-       echo >&2 "cannot acquire lock, giving up on $lockdir"
-  exit 1
+if mkdir "$lockdir" > /dev/null 2>&1
+
+   then
+       # the directory did not exist, but was created successfully
+
+       if [ $TERMINAL_VERBOSE == "1" ] ; then
+          echo >&2 "successfully acquired lock: $lockdir"
+       fi
+   else
+
+       # failed to create the directory, presumably because it already exists
+
+       if [ $TERMINAL_VERBOSE == "1" ] ; then
+          echo >&2 "cannot acquire lock, giving up on $lockdir"
+          echo >&2 "swtor-menu.sh exiting with error-code 1"
+       fi
+       zenity --error --width=600 --text="Lockdirectory can not be created !"
+       exit 1
 fi
 
 
 # On every single startup of Tails, the initial process of the addon has to be run once ...
 
-sleep 5 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Starting the initialisation of the addon !" > /dev/null 2>&1)
+if [ $TERMINAL_VERBOSE == "1" ] ; then
+   echo starting initial-process of the addon
+fi
+
+sleep 5 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Initialisation has started !" > /dev/null 2>&1)
 
 
-if [ ! -f ~/swtor_init ]
-   then
+if [ ! -f ~/swtor_init ] ; then
    ~/Persistent/scripts/init-swtor.sh
-   if [ ! -f ~/swtor_init ]
-      then
+   if [ ! -f ~/swtor_init ] ; then
           sleep 6 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Error during the initialisation of the addon !" > /dev/null 2>&1)
+      if [ $TERMINAL_VERBOSE == "1" ] ; then
+         echo >&2 "initial-process swtor-init.sh has terminated with error-code 1"
+         echo >&2 "swtor-menu.sh exiting with error-code 1"
+      fi
       rmdir $lockdir > /dev/null 2>&1
       exit 1
    fi
 else
-    echo initial-process has ben executed with error-code 0 ...
-    sleep 2 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Iinitialisation of the addon is now complete !" > /dev/null 2>&1)
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo initial-process has ben executed successfully
+    fi
+    sleep 2 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Info" --text="Iinitialisation complete !" > /dev/null 2>&1)
 fi
-
 
 # show version of script prior to show menu
 
+if [ $TERMINAL_VERBOSE == "1" ] ; then
+   echo show info about addon
+fi
+
 cd ~/Persistent/scripts/
-./swtor-about &
-sleep 4
-pid=$(ps | grep swtor-about | awk '{print $1}')
-kill -9 $(echo $pid) > /dev/null 2>&1
+./swtor-about & 2>&1 > /dev/null
+sleep 3
+pkill swtor-about
+
 
 # Build main menu
 
@@ -65,43 +129,63 @@ while [ $menu -eq 1 ]; do
 
        cd ~/Persistent/scripts
 
-       selection=$(zenity --width=600 --height=400 --list --hide-header --title "swtor-addon mainmenu" --column="ID"  --column="" \
-       "1"  "[01]  ->  Select SSH-Server to connect" \
-       "2"  "[02]  ->  Browser for over ssh-socks 5 :Fixed Profile" \
-       "3"  "[03]  ->  Browser for over ssh-socks 5 :Normal Profile" \
-       "4"  "[04]  ->  Browser for over ssh-socks 5 :Anonymous Profile" \
-       "5"  "[05]  ->  Utilitys & Help" \
-       "6"  "[06]  ->  Exit" \
-       --hide-column=1 \
-       --print-column=1)
+       if [ $TERMINAL_VERBOSE == "1" ] ; then
+          echo show main menu of addon
+       fi
 
-if [ "$selection" == "" ] ; then
-    menu=0
-fi
+       if [ ! -d "~/Peristent/personal-files/3" ]
+          then
+            selection=$(zenity --width=600 --height=400 --list --hide-header --title "swtor-addon mainmenu" --column="ID"  --column="" \
+            "1"  "[01]  ->  Select SSH-Server to connect" \
+            "2"  "[02]  ->  RESERVED-FOR-FIXED-PROFILE" \
+            "3"  "[03]  ->  Browser for over ssh-socks 5 :Normal Profile" \
+            "4"  "[04]  ->  Browser for over ssh-socks 5 :Anonymous Profile" \
+            "5"  "[05]  ->  Utilitys & Help" \
+            "6"  "[06]  ->  Exit" \
+            --hide-column=1 \
+            --print-column=1)
+       else
+            selection=$(zenity --width=600 --height=400 --list --hide-header --title "swtor-addon mainmenu" --column="ID"  --column="" \
+            "1"  "[01]  ->  Select SSH-Server to connect" \
+            "2"  "[02]  ->  Browser for over ssh-socks 5 :Fixed Profile" \
+            "3"  "[03]  ->  Browser for over ssh-socks 5 :Normal Profile" \
+            "4"  "[04]  ->  Browser for over ssh-socks 5 :Anonymous Profile" \
+            "5"  "[05]  ->  Utilitys & Help" \
+            "6"  "[06]  ->  Exit" \
+            --hide-column=1 \
+            --print-column=1)
+       fi
 
-
-if [ $selection == "1" ] ; then
-   ./selector.sh 2>&1  > /dev/null 
-fi
-
-if [ $selection == "2" ] ; then
-   ./browser_fix.sh 2>&1 > /dev/null &
-fi
-
-if [ $selection == "3" ] ; then
-   ./browser_normal.sh 2>&1 > /dev/null &
-fi
-
-if [ $selection == "4" ] ; then
-   ./browser_anonymous.sh 2>&1 > /dev/null &
-fi
-
-if [ $selection == "5" ] ; then
-   ./swtor-tools.sh
-fi
-
-if [ $selection == "6" ] ; then
+if [ -z ${selection} ]; then
    menu=0
+else
+   if [ $TERMINAL_VERBOSE == "1" ] ; then
+      echo chosen entry from menu :  $selection
+   fi
+
+   if [ $selection == "1" ] ; then
+      ./selector.sh 2>&1  > /dev/null
+   fi
+
+   if [ $selection == "2" ] ; then
+      ./browser_fix.sh 2>&1 > /dev/null &
+   fi
+
+   if [ $selection == "3" ] ; then
+      ./browser_normal.sh 2>&1 > /dev/null &
+   fi
+
+   if [ $selection == "4" ] ; then
+      ./browser_anonymous.sh 2>&1 > /dev/null &
+   fi
+
+   if [ $selection == "5" ] ; then
+      ./swtor-tools.sh
+   fi
+
+   if [ $selection == "6" ] ; then
+       menu=0
+   fi
 fi
 
 done
@@ -109,9 +193,11 @@ done
 
 # remove lockdir ...
 
-rmdir $lockdir > /dev/null 2>&1 
-echo >&2 "successfully removed lock: $lockdir"
+rmdir $lockdir 2>&1 >/dev/null
 
+if [ $TERMINAL_VERBOSE == "1" ] ; then
+   echo >&2 "successfully removed lock: $lockdir"
+fi
 
 exit 0
 
