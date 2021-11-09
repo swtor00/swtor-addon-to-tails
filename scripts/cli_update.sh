9ebcd88425dea3c297985e7348ca6e159d726abb
swtor00 <amnesia@localhost>
2021-11-09 16:19:05 +0000

    0.60

---
+++ scripts/cli_update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#########################################################
+# SCRIPT  : cli_update.sh                               #
+#########################################################
+# AUTHORS : swtor00                                     #
+# EMAIL   : swtor00@protonmail.com                      #
+# OS      : Tails 4.24 or higher                        #
+#                                                       #
+# VERSION : 0.60                                        #
+# STATE   : BETA                                        #
+#                                                       #
+# This shell script is part of the swtor-addon-to-tails #
+#                                                       #
+# DATE    : 09-11-2021                                  #
+# LICENCE : GPL 2                                       #
+#########################################################
+# Github-Homepage :                                     #
+# https://github.com/swtor00/swtor-addon-to-tails       #
+#########################################################
+
+git reset --hard
+git pull --rebase=preserve --allow-unrelated-histories https://github.com/swtor00/swtor-addon-to-tails
+
+
+
+
+
+
+
+
+
+
