745e4f248496bd3c49fd4c535aa5b4605bed8dbc
0.60 <amnesia@localhost>
2021-11-11 14:26:15 +0000

    0.60

--- scripts/swtor-tweak-gui.sh
+++ scripts/swtor-tweak-gui.sh
@@ -51,7 +51,7 @@ gsettings set org.gtk.Settings.FileChooser show-hidden true
 # use-system-font=false                                 #
 # font='Noto Mono 12'                                   #
 #########################################################
-zenity --question --width=600 --text="Would you like to change the color of the Terminal inside Tails ?\nIf you are working very often with the Terminal I would say yes here, otherwise anwser no.\n\nFor me was the standard contrast of the Terminal of Tails allmost not readable so I made this little change."  > /dev/null 2>&1
+zenity --question --width=600 --text="Would you like to change the color of the Terminal inside Tails ?\nIf you are working very often with the Terminal I would say yes here, otherwise anwser no.\n\nFor me was the Terminal of Tails allmost not readable with the current color, so I made this little change."  > /dev/null 2>&1
 case $? in
          0)
            dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/background-color "'rgb(0,43,54)'"
@@ -79,12 +79,30 @@ esac
 #  picture-uri='file:///home/amnesia/Pictures/Wallpapers/swtor-desktop-freezed.jpeg' #
 ######################################################################################
 
-if [ ! -d ~/Pictures/Wallpapers ] ; then
-   mkdir ~/Pictures/Wallpapers
-   cp ~/Persistent/doc/swtor-desktop-freezed.jpeg ~/Pictures/Wallpapers
-fi
 
-dconf write  /org/gnome/desktop/background/picture-uri "'file:///home/amnesia/Pictures/Wallpapers/swtor-desktop-freezed.jpeg'"
+zenity --question --width=600 --text="Would you like to change the solid background to a picture of the addon ?\n\n"  > /dev/null 2>&1
+case $? in
+         0)
+           if [ ! -d ~/Pictures/Wallpapers ] ; then
+              mkdir ~/Pictures/Wallpapers
+              cp ~/Persistent/doc/swtor-desktop-freezed.jpeg ~/Pictures/Wallpapers
+           fi
+           dconf write  /org/gnome/desktop/background/picture-uri "'file:///home/amnesia/Pictures/Wallpapers/swtor-desktop-freezed.jpeg'"
+           if [ $TERMINAL_VERBOSE == "1" ] ; then
+              echo >&2 "desktop wallpaper changed"
+           fi
+           ;;
+         1)
+           if [ $TERMINAL_VERBOSE == "1" ] ; then
+              echo >&2 "background color remains at it is ... no changing."
+           fi
+           ;;
+esac
+
+
+
+
+
 
 
 
