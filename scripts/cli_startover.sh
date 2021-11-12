f40c6da58205147cf340a9b854411a353f8131f1
swtor00 <amnesia@localhost>
2021-11-12 17:44:11 +0000

    0.60

---
+++ scripts/cli_startover.sh
@@ -0,0 +1,37 @@
+#/bin/bash
+#########################################################
+# SCRIPT  : cli_startover.sh                            #
+#########################################################
+# AUTHORS : swtor00                                     #
+# EMAIL   : swtor00@protonmail.com                      #
+# OS      : Tails 4.24 or higher                        #
+#                                                       #
+#                                                       #
+# VERSION : 0.60                                        #
+# STATE   : BETA                                        #
+#                                                       #
+# This shell script is part of the swtor-addon-to-tails #
+#                                                       #
+# DATE    : 07-11-21                                    #
+# LICENCE : GPL 2                                       #
+#########################################################
+# Github-Homepage :                                     #
+# https://github.com/swtor00/swtor-addon-to-tails       #
+#########################################################
+
+echo "please remove # from the next lines to make a tabula rasa"
+echo "this lines delete all from the script and make a factory-reset"
+
+# rm -rf ~/Persistent/personal
+# unlink ~/Persistent/doc
+# unlink ~/Persistent/scripts
+# unlink ~/Persistent/settings
+# unlink ~/Persistent/swtorcfg
+# unlink ~/Desktop/swtor-menu.sh
+# rm -rf ~/Persistent/swtor-addon-to-tails/swtorcfg
+# cd ~/Persistent/swtor-addon-to-tails
+# git reset --hard > /dev/null 2>&1
+# git pull --rebase=preserve --allow-unrelated-histories https://github.com/swtor00/swtor-addon-to-tails > /dev/null 2>&1
+
+
+
