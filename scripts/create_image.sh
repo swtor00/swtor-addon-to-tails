c95d337a72cee2e52c4a5045e263ea278d8f2653
swtor00 <amnesia@localhost>
2021-12-13 19:35:16 +0000

    0.60

--- scripts/create_image.sh
+++ scripts/create_image.sh
@@ -493,7 +493,6 @@ esac
 
 # Now we have to make a clean tails-repair-disk for this backup
 
-
 if [ ! -d ~/Persistent/personal-files/tails-repair-disk ] ; then
    mkdir ~/Persistent/personal-files/tails-repair-disk
 fi
@@ -505,12 +504,13 @@ echo "#/bin/bash" > restore_from_external.sh
 echo "cp id_rsa ~/.ssh" >> restore_from_external.sh
 echo "cp id_rsa.pub ~/.ssh" >> restore_from_external.sh
 echo "cp known_hosts ~/.ssh"  >> restore_from_external.sh
-echo "chmod 600 ~/.ssh/id_rsa" >> restore_from_external.sh 
-echo "chmod 644 ~/.ssh/*.pub" >> restore_from_external.sh 
-echo "ssh-add" >> restore_from_external.sh 
-echo "sleep 1" >> restore_from_external.sh 
+echo "chmod 600 ~/.ssh/id_rsa" >> restore_from_external.sh
+echo "chmod 644 ~/.ssh/*.pub" >> restore_from_external.sh
+echo "ssh-add" >> restore_from_external.sh
+echo "sleep 1" >> restore_from_external.sh
 echo "rm id_rsa" >> restore_from_external.sh
 echo "rm id_rsa.pub" >> restore_from_external.sh
+echo "rm known_hosts"  >> restore_from_external.sh
 echo "scp -P "$single_port $ssh_host$final_backup_file.md5 . >>  restore_from_external.sh
 echo "scp -P "$single_port $ssh_host$final_backup_file . >>  restore_from_external.sh
 echo "git clone https://github.com/swtor00/swtor-addon-to-tails"  >>  restore_from_external.sh
