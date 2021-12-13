e0c0d3134664ecde2013f9bb6c94d86be3669923
swtor00 <amnesia@localhost>
2021-12-13 16:20:31 +0000

    0.60

--- scripts/update.sh
+++ scripts/update.sh
@@ -25,6 +25,15 @@ if [ "$TERMINAL_VERBOSE" == "" ];then
    exit 1
 fi
 
+swtor_connected
+if [ $? -eq 0 ] ; then
+   if [ $TERMINAL_VERBOSE == "1" ] ; then
+      echo >&2 "connection check executed"
+   fi
+else
+    exit 1
+fi
+
 zenity --question --width=600 --text="Please read this warning carefully.\n\nIf you update the addon, all local changes made by you,will be overwriten.\n\This also includes the configuration file swtor.cfg and all scripts.\n\nAre you sure, you would like to proceed with the update ?" > /dev/null 2>&1
 case $? in
          0)
@@ -32,9 +41,8 @@ case $? in
 
            sleep 10 | tee >(zenity --progress --pulsate --no-cancel --auto-close --title="Information" --text="\n\nThe update is now executed. Please wait ! \n\n" > /dev/null 2>&1)
 
-           show_wait_dialog && sleep 2 
+           show_wait_dialog && sleep 2
 
-   
            git reset --hard > /dev/null 2>&1
            git pull --rebase=preserve --allow-unrelated-histories https://github.com/swtor00/swtor-addon-to-tails > /dev/null 2>&1
 
