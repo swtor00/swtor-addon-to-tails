33a25d735ffdcce984f204de78d35fb404dfa5ff
swtor00 <amnesia@localhost>
2022-03-06 11:20:26 +0000

    0.60

---
+++ scripts/cli_remove_lockdir.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+#########################################################
+# SCRIPT  : cli_remove_lockdir.sh                       #
+#########################################################
+# AUTHORS : swtor00                                     #
+# EMAIL   : swtor00@protonmail.com                      #
+# OS      : Tails 4.27 or higher                        #
+#                                                       #
+# VERSION : 0.60                                        #
+# STATE   : BETA                                        #
+#                                                       #
+# This shell script is part of the swtor-addon-to-tails #
+#                                                       #
+# DATE    : 05-03-2022                                  #
+# LICENCE : GPL 2                                       #
+#########################################################
+# Github-Homepage :                                     #
+# https://github.com/swtor00/swtor-addon-to-tails       #
+#########################################################
+
+rm ~/Persistent/scripts/menu.lock > /dev/null 2>&1
