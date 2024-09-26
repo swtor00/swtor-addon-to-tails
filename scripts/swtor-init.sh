14c531641934f3915319376c1e1640bc6dec19cc
swtor00 <amnesia@localhost>
2024-09-26 17:26:34 +0000

    0.83

---
+++ scripts/swtor-init.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+#########################################################
+# SCRIPT  : swtor-init.sh                               #
+#########################################################
+# AUTHORS : swtor00                                     #
+# EMAIL   : swtor00@protonmail.com                      #
+# OS      : Tails 6.7 or higher                         #
+#                                                       #
+# VERSION : 0.83                                        #
+# STATE   : BETA                                        #
+#                                                       #
+#                                                       #
+# DATE    : 26-09-2024                                  #
+# LICENCE : GPL 2                                       #
+#########################################################
+# Github-Homepage :                                     #
+# https://github.com/swtor00/swtor-addon-to-tails       #
+#########################################################
+
+
+if grep -q "IMPORT-BOOKMARKS:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export IMPORT_BOOKMAKRS="1"
+else
+   export IMPORT_BOOKMAKRS="0"
+fi
+
+if grep -q "GUI-LINKS:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export GUI_LINKS="1"
+else
+   export GUI_LINKS="0"
+fi
+
+if grep -q "CHECK-UPDATE:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export CHECK_UPDATE="1"
+else
+   export CHECK_UPDATE="0"
+fi
+
+if grep -q "BACKUP-FIXED-PROFILE:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+   export BACKUP_FIXED_PROFILE="1"
+else
+   export BACKUP_FIXED_PROFILE="0"
+fi
+
+if grep -q "BACKUP_APT_LIST:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export BACKUP_APT_LIST="1"
+else
+     export BACKUP_APT_LIST="0"
+fi
+
+if grep -q "TERMINAL-VERBOSE:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export TERMINAL_VERBOSE="1"
+else
+     export TERMINAL_VERBOSE="0"
+fi
+
+if grep -q "BROWSER-SOCKS5:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export BROWSER_SOCKS5="1"
+else
+     export BROWSER_SOCKS5="0"
+fi
+
+if grep -q "BYPASS-SOFTWARE-CHECK:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export BYPASS="1"
+else
+     export BYPASS="0"
+fi
+
+if grep -q "CHECK-EMPTY-SSH:NO" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export CHECK_SSH="0"
+else
+     export CHECK_SSH="1"
+fi
+
+if grep -q "AUTOCLOSE-BROWSER:YES" ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg ; then
+     export AUTOCLOSE_BROWSER="1"
+else
+     export AUTOCLOSE_BROWSER="0"
+fi
+
+
+export TIMEOUT_TB=$(grep TIMEOUT-TB ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg | sed 's/[A-Z:-]//g')
+export TIMEOUT_SSH=$(grep TIMEOUT-SSH ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg | sed 's/[A-Z:-]//g')
+export XCLOCK_SIZE=$(grep XCLOCK-SIZE ~/Persistent/swtor-addon-to-tails/swtorcfg/swtor.cfg | sed 's/[A-Z:-]//g')
+
+export  DEBUGW="0"
+
+
+source ~/Persistent/scripts/swtor-global.sh
+global_init
+if [ $? -eq 0 ] ; then
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo >&2 "global_init() done"
+    fi
+else
+    if [ $TERMINAL_VERBOSE == "1" ] ; then
+       echo >&2 "failure during initialisation of global-init() !"
+       echo >&2 "swtor-init.sh exiting with error-code 1"
+    fi
+    exit 1
+fi
+
+# Creating the lockdirectory ....
+
+lockdir=~/Persistent/scripts/init.lock
+if mkdir "$lockdir" > /dev/null 2>&1 then
+
+       # the directory did not exist, but was created successfully
+
+       if [ $TERMINAL_VERBOSE == "1" ] ; then
+          echo >&2 "successfully acquired lock: $lockdir"
+       fi
+
+else
+
+       # failed to create the directory, presumably because it already exists
+
+       if [ $TERMINAL_VERBOSE == "1" ] ; then
+          echo >&2 "cannot acquire lock, giving up on $lockdir"
+          echo >&2 "swtor-menu.sh exiting with error-code 1"
+       fi
+       zenity --error --width=600 --text="Lockdirectory for initialisation can not be created !"
+       exit 1
+fi
+
+
+
+# remove lockdir ...
+
+rmdir $lockdir 2>&1 >/dev/null
+
+exit 0
+
