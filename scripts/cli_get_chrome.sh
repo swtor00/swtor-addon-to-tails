20d199f0829a2c7c8f93475c3d03c1b23448aaf7
swtor00 <swtor00@protonmail.com>
2025-09-26 12:04:56 +0000

    Tails 7.0

---
+++ scripts/cli_get_chrome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#########################################################
+# SCRIPT  : cli_get_chrome.sh                           #
+#########################################################
+# AUTHORS : swtor00                                     #
+# EMAIL   : swtor00@protonmail.com                      #
+# OS      : Tails 7.0 or higher                         #
+#                                                       #
+# VERSION : 0.85                                        #
+# STATE   : BETA                                        #
+#                                                       #
+# This shell script is part of the swtor-addon-to-tails #
+#                                                       #
+# DATE    : 26-09-2025                                  #
+# LICENCE : GPL 2                                       #
+#########################################################
+# Github-Homepage :                                     #
+# https://github.com/swtor00/swtor-addon-to-tails       #
+#########################################################
+
+wget -O ~/Persistent/swtor-addon-to-tails/deb/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+exit 0
+
+
+
+
+
